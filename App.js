5ea6d862eedcbba52e8906d19746f1b96de82cad
fabiopassos21 <fabiopassos1997@gmail.com>
2022-12-31 18:43:46 -0300

    FlatList

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList
+  } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Pessoas from './src/Pessoas/Pessoas';
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      lista: [
+        {id: '1', nome:'Matheus', idade:50, email:'matheus@matheus.com'},
+        {id: '2', nome:'Thiago', idade:33, email:'thiago@thiago.com'},
+        {id: '3', nome:'Lucas', idade:20, email:'lucas@lucas.com'},
+        {id: '4', nome:'Henrique', idade:50, email:'Henrique@Henrique.com'},
+        {id: '5', nome:'Thiago', idade:33, email:'thiago@thiago.com'},
+        {id: '6', nome:'Lucas', idade:20, email:'lucas@lucas.com'},
+        {id: '7', nome:'JOSE', idade:33, email:'thiago@thiago.com'},
+        {id: '8', nome:'HENRIQUE', idade:20, email:'lucas@lucas.com'},
+
+      ]
+    };
+  }
+
+  render(){
+    return(
+      <View style={styles.container}> 
+
+      <FlatList
+      data={this.state.lista}
+      keyExtractor={(item)=> item.id}
+
+      renderItem={ ({item}) => <Pessoas data={item} /> }
+      />
+
+      </View>    
+    );
+  }
+
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1,
   },
 });
+
+export default App;
+
+
